c1f98438a58679b09405dc92c107d9f8d43d896e
João Paulo <paulocorrentinense@gmail.com>
2024-12-09 07:42:46 -0300

    middlewares

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -1,7 +1,31 @@
 const { body } = require("express-validator")
 
 const userCreateValidation = () => {
-    return [body("name").isString().withMessage("O nome é obrigatório.")]
+    return [
+        body("name")
+            .isString().withMessage("O nome é obrigatório.")
+            .isLength({ min: 3 }).withMessage("O nome precisa ter  mínimo três caracteres."),
+
+        body("email")
+            .isString().withMessage("O e-mail é obrigatório.")
+            .isEmail().withMessage("Insira um e-mail válido."),
+
+        body("password")
+            .isString().withMessage("A senha é obrigatório.")
+            .isLength({ min: 5 }).withMessage("A senha precisa ter  mínimo cinco caracteres."),
+
+        body("confirmpassword")
+            .isString().withMessage("A confirmação de senha é obrigatório.")
+            .custom((value, {req}) => {
+                if(value != req.body.password) {
+                    throw new Error("As senhas não são iguais.")
+                }
+                return true
+            })
+
+
+
+    ]
 }
 
 module.exports = {
